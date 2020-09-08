70e3f3ce5cf554d4121b376c6a68b64948efe127
cotrones <tomcotroneo@gmail.com>
2020-09-07 23:22:09 -0400

    style books and build modal with form to add book

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,41 @@ function addBookToLibrary(title, author, pages, read) {
 function displayLibrary() {
 	
 }
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-modal-close]');
+const overlay = document.getElementById('overlay');
+const submitButton = document.getElementById('submit-button');
+
+openModalButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		const modal = document.querySelector(button.dataset.modalTarget);
+		openModal(modal);
+	});
+});
+
+closeModalButtons.forEach(button => {
+	button.addEventListener('click', () => {
+		const modal = button.closest('.modal');
+		closeModal(modal);
+	});
+});
+
+overlay.addEventListener('click', () => {
+	const modals = document.querySelectorAll('.modal.active');
+	modals.forEach(modal => {
+		closeModal(modal);
+	})
+});
+
+function openModal(modal) {
+	if (modal == null) return;
+	modal.classList.add('active');
+	overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+	if (modal == null) return;
+	modal.classList.remove('active');
+	overlay.classList.remove('active');
+}
